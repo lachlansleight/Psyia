e667094e6d9e1baa5b3d738ab300bf197772c9f1
lachlansleight <lachlan@liminalvr.com>
2017-05-20 23:33:10 +1000

    Made Menu button quit

---
+++ Assets/Scripts/Th_er_MenuQuits.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Th_er_MenuQuits : MonoBehaviour {
+	
+	void Update () {
+		if(VRTools.VRInput.GetDevice("ViveLeft").GetButtonDown("Menu") || VRTools.VRInput.GetDevice("ViveRight").GetButtonDown("Menu")) Application.Quit();
+	}
+}
