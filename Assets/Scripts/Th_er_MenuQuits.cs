e483a6404496a7a10db14995ad95d4170a1c93f9
lachlansleight <lachlan@liminalvr.com>
2018-02-25 13:04:11 +1100

    Wiped everything

--- Assets/Scripts/Th_er_MenuQuits.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Th_er_MenuQuits : MonoBehaviour {
-	
-	void Update () {
-		if(VRTools.VRInput.GetDevice("ViveLeft").GetButtonDown("Menu") || VRTools.VRInput.GetDevice("ViveRight").GetButtonDown("Menu")) Application.Quit();
-	}
-}
