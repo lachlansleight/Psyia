7fa41ae6b7d21a9a32909eacfe0aafe0fa86df11
lachlansleight <lachlan@liminalvr.com>
2018-06-11 15:30:19 +1000

    Reduce size with controller distance

---
+++ Assets/Psyia/Scripts/ControllerForceDistanceMod.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ControllerForce))]
+public class ControllerForceDistanceMod : MonoBehaviour {
+
+	public Transform Target;
+	public AnimationCurve ModifierCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+	private ControllerForce CF;
+
+	void Awake() {
+		CF = GetComponent<ControllerForce>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float CurrentDistance = Vector3.Magnitude(transform.position - Target.position);
+		CF.MaxForce = ModifierCurve.Evaluate(CurrentDistance);
+	}
+}
