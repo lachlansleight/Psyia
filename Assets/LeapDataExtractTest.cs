abd3cc57151a9e9068a2f10fe361fc5996157590
Lachlan Sleight <lachlansleight@gmail.com>
2018-07-02 11:20:14 +1000

    Added simple Psyia force
    It's great. Make a fist to stop attracting particles, open the hand to attract.
    (Not ideal from a gestural point of view, but don't have great alternatives...)

---
+++ Assets/LeapDataExtractTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+
+
+public class LeapDataExtractTest : MonoBehaviour {
+
+	public HandModelBase TargetHand;
+
+	public Transform DebugPalm;
+	public Transform[] DebugFingers;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Leap.Hand hand = TargetHand.GetLeapHand();
+		DebugPalm.position = hand.GetPalmPose().position;
+		DebugPalm.rotation = hand.GetPalmPose().rotation;
+		DebugPalm.localScale = Vector3.one * Mathf.Lerp(0.1f, 0.01f, hand.GetFistStrength());
+
+		List<Leap.Finger> fingers = hand.Fingers;
+		for(int i = 0; i < fingers.Count; i++) {
+			DebugFingers[i].position = fingers[i].TipPosition.ToVector3();
+			DebugFingers[i].rotation = Quaternion.LookRotation(fingers[i].Direction.ToVector3(), Vector3.up);
+		}
+	}
+}
---
+++ Assets/Psyia/Scripts/LeapHandForce.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+[RequireComponent(typeof(ForceSource))]
+public class LeapHandForce : MonoBehaviour {
+
+	public HandModelBase TargetHand;
+	public float MinFistThreshold = 0.1f;
+	public float MaxFistThreshold = 0.9f;
+
+	private ForceSource _Source;
+	private ForceSource Source {
+		get {
+			if(_Source == null) {
+				_Source = GetComponent<ForceSource>();
+			}
+			return _Source;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!TargetHand.IsTracked) {
+			Source.StrengthModifier = 0f;
+			return;
+		}
+		Hand hand = TargetHand.GetLeapHand();
+		if(hand.Confidence == 0f) {
+			Source.StrengthModifier = 0f;
+			return;
+		}
+		transform.position = hand.GetPalmPose().position;
+		transform.rotation = Quaternion.LookRotation(hand.PalmNormal.ToVector3(), Vector3.up);
+		Source.StrengthModifier = Mathf.Lerp(1f, 0f, Mathf.InverseLerp(MinFistThreshold, MaxFistThreshold, hand.GetFistStrength()));
+	}
+}
