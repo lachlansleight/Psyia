e8fefd026cef9177b8026b2c4e5ee837b27b09b4
Lachlan Sleight <lachlansleight@gmail.com>
2018-12-09 14:28:54 +1100

    Added particle attraction and spawning

---
+++ Assets/AppContent/Scripts/ControllerForce.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Psyia;
+using UnityEngine;
+using Valve.VR;
+
+[RequireComponent(typeof(PsyiaForce))]
+public class ControllerForce : MonoBehaviour
+{
+
+	public SteamVR_Action_Pose PoseAction;
+	public SteamVR_Action_Single ApplyForceAction;
+	public SteamVR_Input_Sources Hand;
+	
+	[Range(0f, 1f)] public float Value;
+
+	private PsyiaForce _psyiaForce;
+
+	private void Awake()
+	{
+		_psyiaForce = GetComponent<PsyiaForce>();
+	}
+	
+	// Use this for initialization
+	public void OnEnable ()
+	{
+		ApplyForceAction.AddOnChangeListener(OnAxisValueChanged, Hand);
+		PoseAction.AddOnChangeListener(OnPoseChanged, Hand);
+	}
+
+	public void OnDisable()
+	{
+		ApplyForceAction.RemoveOnChangeListener(OnAxisValueChanged, Hand);
+	}
+	
+	// Update is called once per frame
+	public void Update ()
+	{
+		//LeftValue = SteamVR_Input._default.inActions.ApplyForce.GetAxis(SteamVR_Input_Sources.LeftHand);
+	}
+
+	private void OnAxisValueChanged(SteamVR_Action_In actionIn)
+	{
+		if (!(actionIn is SteamVR_Action_Single)) return;
+		var asSingle = (SteamVR_Action_Single) actionIn;
+		Value = asSingle.GetAxis(Hand);
+		_psyiaForce.StrengthMultiplier = Value;
+	}
+
+	private void OnPoseChanged(SteamVR_Action_In actionIn)
+	{
+		if (!(actionIn is SteamVR_Action_Pose)) return;
+		var asPose = (SteamVR_Action_Pose) actionIn;
+		transform.position = asPose.GetLocalPosition(Hand);
+		transform.rotation = Quaternion.Inverse(asPose.GetLocalRotation(Hand));
+	}
+}
---
+++ Assets/AppContent/Scripts/ControllerSpawn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Psyia;
+using UnityEngine;
+using Valve.VR;
+
+[RequireComponent(typeof(PsyiaEmitter))]
+public class ControllerSpawn : MonoBehaviour {
+
+	public SteamVR_Action_Pose PoseAction;
+	public SteamVR_Action_Single SpawnParticlesAction;
+	public SteamVR_Input_Sources Hand;
+	
+	[Range(0f, 1f)] public float Value;
+
+	private PsyiaEmitter _psyiaEmitter;
+
+	private void Awake()
+	{
+		_psyiaEmitter = GetComponent<PsyiaEmitter>();
+	}
+	
+	// Use this for initialization
+	public void OnEnable ()
+	{
+		SpawnParticlesAction.AddOnChangeListener(OnAxisValueChanged, Hand);
+		PoseAction.AddOnChangeListener(OnPoseChanged, Hand);
+	}
+
+	public void OnDisable()
+	{
+		SpawnParticlesAction.RemoveOnChangeListener(OnAxisValueChanged, Hand);
+	}
+	
+	// Update is called once per frame
+	public void Update ()
+	{
+		//LeftValue = SteamVR_Input._default.inActions.ApplyForce.GetAxis(SteamVR_Input_Sources.LeftHand);
+	}
+
+	private void OnAxisValueChanged(SteamVR_Action_In actionIn)
+	{
+		if (!(actionIn is SteamVR_Action_Single)) return;
+		var asSingle = (SteamVR_Action_Single) actionIn;
+		Value = Mathf.InverseLerp(0.2f, 1f, asSingle.GetAxis(Hand));
+		_psyiaEmitter.EmissionMultiplier = Value;
+		_psyiaEmitter.Settings.MinSpawnVelocity = Mathf.Lerp(0f, 0.005f, Value);
+		_psyiaEmitter.Settings.MaxSpawnVelocity = Mathf.Lerp(0f, 0.001f, Value);
+	}
+
+	private void OnPoseChanged(SteamVR_Action_In actionIn)
+	{
+		if (!(actionIn is SteamVR_Action_Pose)) return;
+		var asPose = (SteamVR_Action_Pose) actionIn;
+		transform.position = asPose.GetLocalPosition(Hand);
+		transform.rotation = Quaternion.Inverse(asPose.GetLocalRotation(Hand));
+	}
+}
