5c91b9a1ef26e406f592269dcc5d83c7267c2a8a
Lachlan Sleight <lachlansleight@gmail.com>
2019-06-27 20:52:05 +1000

    Cut and run

---
+++ Assets/_Scripts/ModeMenu/MenuChoreography.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuChoreography : MonoBehaviour
+{
+	public void Start()
+	{
+		
+	}
+
+	public void Update()
+	{
+		
+	}
+}
